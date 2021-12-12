aea54d6e60574ffeeadfc1082dae70b8ea734269
polinaantropova <91796705+polinaantropova@users.noreply.github.com>
2021-12-12 10:42:43 +0300

    Create 8kyu_Returning_Strings.cpp

---
+++ 8kyu_Returning_Strings.cpp
@@ -0,0 +1,10 @@
+/*Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+[Make sure you type the exact thing I wrote or the program may not execute properly]*/
+
+
+std::string greet(const std::string& n){
+  //your code here
+std::string line="";
+  line="Hello, "+n+" how are you doing today?";
+return line;
+}
