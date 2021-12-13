fc4862b8674c89c8f5a0999b86048739da83cafc
polinaantropova <91796705+polinaantropova@users.noreply.github.com>
2021-12-13 23:28:03 +0300

    Create 8kyu_Even_or_Odd.cpp

---
+++ tasks/8kyu_Even_or_Odd.cpp
@@ -0,0 +1,8 @@
+/*Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.*/
+
+#include <string>
+
+std::string even_or_odd(int number) 
+{
+  return (number%2==0)?"Even":"Odd";
+}
