7a4b4e7bfaebbb887f85b9b10b9bd1d04a5bafd7
polinaantropova <91796705+polinaantropova@users.noreply.github.com>
2021-12-14 11:22:41 +0300

    Create 6kyu_A_disguised_sequence_(I).cpp

---
+++ tasks/6kyu_A_disguised_sequence_(I).cpp
@@ -0,0 +1,26 @@
+/*Given u0 = 1, u1 = 2 and the relation 6unun+1-5unun+2+un+1un+2 = 0 calculate un for any integer n >= 0.
+
+Examples:
+Call fcn the function such as fcn(n) = un.
+
+fcn(17) -> 131072; fcn(21) -> 2097152
+
+Remark:
+You can take two points of view to do this kata:
+
+the first one purely algorithmic from the definition of un
+
+the second one - not at all mandatory, but as a complement - is to get a bit your head around and find which sequence is hidden behind un.*/
+
+//u0=1; u1=2; u2=4; u3=8; .... - прогрессия : 2^n 
+
+
+#include<cmath>
+class HiddenSeq
+{
+    public:
+    static unsigned long long fcn(int n){
+return std::pow(2,n);
+}
+
+};
