896578f3121068a2c38a5afe83f9c63fa5fda465
polinaantropova <91796705+polinaantropova@users.noreply.github.com>
2021-12-14 17:48:22 +0300

    Create 6kyu_1n- Cycle.cpp

---
+++ tasks/6kyu_1n- Cycle.cpp
@@ -0,0 +1,24 @@
+/*Let be n an integer prime with 10 e.g. 7.
+
+1/7 = 0.142857 142857 142857 ....
+
+We see that the decimal part has a cycle: 142857. The length of this cycle is 6. In the same way:
+
+1/11 = 0.09 09 09 .... Cycle length is 2.
+Task
+Given an integer n (n > 1) the function cycle(n) returns the length of the cycle if there is one otherwise (n and 10 not coprimes) return -1.
+*/
+
+int cycle(int n) {
+    
+    if (n % 5 == 0 || n % 2 == 0) return -1;
+    int num = 1;
+    int symb = 0;
+    while ((num != 1) || (symb == 0)) {
+        num *= 10;
+        num %= n;
+        symb++;
+    }
+    return symb;
+
+}
